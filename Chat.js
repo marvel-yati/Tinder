13ab0085ca83dd682c7c8b45af23247dd98e020e
marvel-yati <56108123+marvel-yati@users.noreply.github.com>
2022-11-13 16:18:14 +0530

    Delete Chat.js

--- Chat.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const chatSchema = mongoose.Schema({
-    name: {type:String},
-    image: {type: String},
-    message: {type: String},
-})
-
-export default mongoose.model("chat", chatSchema);
