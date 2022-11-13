54ad67774867568e2759fc0e98b0064f96323c6d
marvel-yati <56108123+marvel-yati@users.noreply.github.com>
2022-11-13 16:18:24 +0530

    Delete dbCards.js

--- dbCards.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const cardSchema = mongoose.Schema({
-    name: {type:String},
-    imgUrl: {type: String},
-})
-
-export default mongoose.model("cards", cardSchema);
